1405df1369e3a8c1b56cdd066626b130b4f9a78b
Deven Phillips <josphill@redhat.com>
2022-10-01 09:54:20 -0400

    Comment out dependencies and implementations

--- src/main/java/com/redhat/consulting/acna2022/AggregateRoute.java
+++ src/main/java/com/redhat/consulting/acna2022/AggregateRoute.java
@@ -12,16 +12,16 @@ public class AggregateRoute extends RouteBuilder {
 	
 	@Override
 	public void configure() throws Exception {
-		from("timer:aggregate?delay=0&period=10000")
-				.setBody(constant("SELECT COUNT(1) as count FROM tweets WHERE timestamp >= (now() - interval '10 seconds')"))
-				.to("jdbc:default?outputType=StreamList")
-				.split(body())
-				.process(e -> {
-					var rs = e.getIn().getBody(LinkedHashMap.class);
-					var count = (Long)rs.getOrDefault("count", 0);
-					var json = new JsonObject().put("count", count).put("timestamp", Instant.now().toEpochMilli());
-					e.getIn().setBody(json.encodePrettily());
-				})
-				.to("vertx:com.redhat.consulting.aggregate?pubSub=true");
+//		from("timer:aggregate?delay=0&period=10000")
+//				.setBody(constant("SELECT COUNT(1) as count FROM tweets WHERE timestamp >= (now() - interval '10 seconds')"))
+//				.to("jdbc:default?outputType=StreamList")
+//				.split(body())
+//				.process(e -> {
+//					var rs = e.getIn().getBody(LinkedHashMap.class);
+//					var count = (Long)rs.getOrDefault("count", 0);
+//					var json = new JsonObject().put("count", count).put("timestamp", Instant.now().toEpochMilli());
+//					e.getIn().setBody(json.encodePrettily());
+//				})
+//				.to("vertx:com.redhat.consulting.aggregate?pubSub=true");
 	}
 }
--- src/main/java/com/redhat/consulting/acna2022/TwitterIngestRoute.java
+++ src/main/java/com/redhat/consulting/acna2022/TwitterIngestRoute.java
@@ -1,16 +1,9 @@
 package com.redhat.consulting.acna2022;
 
-import io.vertx.core.Vertx;
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jsonb.JsonbDataFormat;
-import org.apache.camel.component.twitter.search.TwitterSearchComponent;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
-import twitter4j.Status;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import java.util.concurrent.atomic.AtomicLong;
 
 import static java.lang.String.format;
@@ -20,44 +13,36 @@ public class TwitterIngestRoute extends RouteBuilder {
 	
 	private static final Logger LOG = Logger.getLogger(TwitterIngestRoute.class);
 	
-//	String searchTerms = "#ACNA2022,@ApacheCamel,@ApacheCon,@TheASF,@InfoSec812,@ApacheGroovy,@QuarkusIO";
-	String searchTerms = "Trump";
+	String searchTerms = "#ACNA2022,@ApacheCamel,@ApacheCon,@TheASF,@InfoSec812,@ApacheGroovy,@QuarkusIO";
 	
 	String twitterPollDelay = "5s";
 	
 	private AtomicLong lastId = new AtomicLong(0L);
 	
-	RouteBuilder currentRoute;
-	
-	@Inject
-	Vertx vertx;
-	
-	@Inject
-	CamelContext context;
-	
 	public void configure() throws Exception {
 		
-		fromF("twitter-search:%s?greedy=true&type=direct&delay=%s", searchTerms, twitterPollDelay)
-				.process(e -> {
-					var s = e.getIn().getBody(Status.class);
-					e.getIn().setHeader("tid", s.getId());
-					e.getIn().setHeader("content", s.getText());
-					e.getIn().setHeader("handle", s.getUser().getScreenName());
-					e.getIn().setHeader("url", format("https://twitter.com/%s/status/%s", s.getUser().getScreenName(), s.getId()));
-					e.getIn().setBody("INSERT INTO tweets (id, content, handle, url) VALUES (:?tid, :?content, :?handle, :?url)");
-				})
-				.to("jdbc:default?useHeadersAsParameters=true")
-				.to("log:debugQuery")
-				.delay(simple("${random(100,1000)}"))
-				.setBody(simple("""
-            {
-              "id": "${header.tid}",
-              "url": "${header.url}",
-              "content": "${header.content}",
-              "handle": "${header.handle}"
-            }
-						"""))
-				.to("vertx:com.redhat.consulting.tweet?pubSub=true")
-				.to("log:TwitterFeedRoute");
+//		fromF("twitter-search:%s?greedy=true&type=direct&delay=%s", searchTerms, twitterPollDelay)
+//				.to("log:rawTweet");
+//				.process(e -> {
+//					var s = e.getIn().getBody(Status.class);
+//					e.getIn().setHeader("tid", s.getId());
+//					e.getIn().setHeader("content", s.getText());
+//					e.getIn().setHeader("handle", s.getUser().getScreenName());
+//					e.getIn().setHeader("url", format("https://twitter.com/%s/status/%s", s.getUser().getScreenName(), s.getId()));
+//					e.getIn().setBody("INSERT INTO tweets (id, content, handle, url) VALUES (:?tid, :?content, :?handle, :?url)");
+//				})
+//				.to("jdbc:default?useHeadersAsParameters=true")
+//				.to("log:debugQuery")
+//				.delay(simple("${random(100,1000)}"))
+//				.setBody(simple("""
+//            {
+//              "id": "${header.tid}",
+//              "url": "${header.url}",
+//              "content": "${header.content}",
+//              "handle": "${header.handle}"
+//            }
+//						"""))
+//				.to("vertx:com.redhat.consulting.tweet?pubSub=true")
+//				.to("log:TwitterFeedRoute");
 	}
 }
